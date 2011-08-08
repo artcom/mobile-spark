4173fcd9df8af595bde2063cc9f88a8bb7e361c8
unknown <valentin.schunack@artcom.de>
2011-08-08 15:14:25 +0200

    (MobileSpark) added exception string_functions unittest (not fully working)

---
+++ core/src/base/Exception.cpp
@@ -0,0 +1,101 @@
+/* __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
+//
+// Copyright (C) 1993-2008, ART+COM AG Berlin, Germany <www.artcom.de>
+//
+// This file is part of the ART+COM Standard Library (asl).
+//
+// It is distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+// __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
+//
+//    $RCSfile: Exception.cpp,v $
+//
+//   $Revision: 1.3 $
+//
+// Description: Exception class hierarchy
+//
+//
+// __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
+*/
+
+
+//own header
+#include "Exception.h"
+
+#include <cstdlib>
+
+#include "string_functions.h"
+//#include "os_functions.h"
+
+//#include "ExitHandler.h"
+
+using namespace std;
+
+namespace asl {
+
+
+    //ExitHandler _ourExitHandler;
+
+    // TODO: make this a static function
+    bool abortOnThrow() {
+        bool myDummy;
+        static bool myAbortOnThrowFlag = false;//asl::get_environment_var_as("AC_ABORT_ON_THROW", myDummy);
+        return myAbortOnThrowFlag;
+    }
+    Exception::Exception(const std::string & what, const std::string & where)
+            : _what(what), _where(where), _name("Exception")
+    {
+        if (abortOnThrow()) {
+            cerr << *this;
+            abort();
+        }
+    };
+
+    Exception::Exception(const std::string & what, const std::string & where, const char * name)
+            : _what(what), _where(where), _name(name)
+    {
+        if (abortOnThrow()) {
+            cerr << *this;
+            abort();
+        }
+    };
+
+    void
+    Exception::initExceptionBehaviour() {
+    }
+}
+
+string
+asl::file_string(const char* file_name) {
+    std::string myFilename(file_name); //asl::toLowerCase(file_name));
+    std::string::size_type mySlash = myFilename.find_last_of("/\\");
+    if (mySlash != std::string::npos) {
+        myFilename = myFilename.substr(mySlash+1);
+    }
+    return myFilename;
+}
+
+string
+asl::line_string(unsigned line_number) {
+    std::string message;
+    message = as_string(line_number);
+    return message;
+}
+
+string
+asl::location_string(const char* file_name, unsigned line_number) {
+    return string("[") + file_string(file_name) + ":" + as_string(line_number) + "]";
+}
+
+string
+asl::compose_message(const Exception& ex) {
+    std::string myMsg;
+    myMsg =
+        string(ex.name() ? ex.name() : "Unspecified Exception") +
+        string(": ") +
+        string(ex.what().size() ? ex.what() : string("Unspecified reason")) +
+        string(" ") +
+        string(ex.where().size() ? ex.where() : string("Unspecified origin"));
+    return myMsg;
+}
