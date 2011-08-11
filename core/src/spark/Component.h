05e32ba24a6168b38e43b1f1e8a7ad379c3aed64
katjae <katjae@artcom.de>
2011-08-11 11:45:46 +0200

    added first spark version, not yet used

---
+++ core/src/spark/Component.h
@@ -0,0 +1,33 @@
+#ifndef _included_mobile_spark_Component_
+#define _included_mobile_spark_Component_
+
+#include <string>
+
+#include <boost/smart_ptr/shared_ptr.hpp>
+
+#include "../base/XMLNode.h"
+
+using namespace asl;
+
+namespace spark {
+
+    class Component;
+    typedef boost::shared_ptr<Component> ComponentPtr;
+
+    class Component {
+    public: 
+        Component();
+        Component(const XMLNodePtr theXMLNode, ComponentPtr theParent);
+        virtual ~Component();
+        virtual void render() const ;
+        
+    protected:
+        const XMLNodePtr _myXMLNode;
+        std::string _myName;
+        virtual void drawObject() const;
+
+    private:
+        ComponentPtr _myParent;
+    };
+};
+#endif 
---
+++ core/src/spark/Widget.h
@@ -0,0 +1,29 @@
+#ifndef _included_mobile_spark_Widget_
+#define _included_mobile_spark_Widget_
+
+#include <string>
+
+#include "../base/MatrixStack.h"
+
+#include "Container.h"
+
+namespace spark {
+
+    class Widget : public Container {
+    public: 
+        Widget(const BaseAppPtr theApp, const XMLNodePtr theXMLNode, ComponentPtr theParent);
+        virtual ~Widget();
+        virtual void render() const;
+    protected:
+        MatrixStack _myLocalMatrixStack; //scale, roation and translation of this node
+    private:
+        float _x,_y,_z;
+        float _scaleX, _scaleY, _scaleZ;
+        float _rotationX, _rotationY, _rotationZ;
+        float _alpha;
+        bool _visible;
+    };
+
+    typedef boost::shared_ptr<Widget> WidgetPtr;
+};
+#endif
