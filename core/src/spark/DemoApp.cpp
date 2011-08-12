2762a4bad1cc8189f4010542c0d6456930941861
katjae <katjae@artcom.de>
2011-08-12 14:51:07 +0200

    fixed jni binding and start script

--- core/src/spark/DemoApp.cpp
+++ core/src/spark/DemoApp.cpp
@@ -16,24 +16,24 @@ spark::DemoApp ourApp;
 #ifdef __ANDROID__
 /////////////////////////////////////////////////////////////////////////JNI
 extern "C" {
-    JNIEXPORT void JNICALL Java_com_artcom_mobile_BaseNativeLib_setup(JNIEnv * env, jobject obj,  
+    JNIEXPORT void JNICALL Java_com_artcom_mobile_Base_NativeBinding_setup(JNIEnv * env, jobject obj,  
                                                                  jstring apkFile,
                                                                  jstring layoutFile);
-    JNIEXPORT void JNICALL Java_com_artcom_mobile_BaseNativeLib_onFrame(JNIEnv * env, jobject obj);
-    JNIEXPORT void JNICALL Java_com_artcom_mobile_BaseNativeLib_onTouch(JNIEnv * env, jobject obj);
+    JNIEXPORT void JNICALL Java_com_artcom_mobile_Base_NativeBinding_onFrame(JNIEnv * env, jobject obj);
+    JNIEXPORT void JNICALL Java_com_artcom_mobile_Base_NativeBinding_onTouch(JNIEnv * env, jobject obj);
 };
 
-JNIEXPORT void JNICALL Java_com_artcom_mobile_BaseNativeLib_setup(JNIEnv * env, jobject obj,  
+JNIEXPORT void JNICALL Java_com_artcom_mobile_Base_NativeBinding_setup(JNIEnv * env, jobject obj,  
                                                              jstring apkFile,
                                                              jstring layoutFile) {
     ourApp.setup(apkFile, layoutFile, env);
 }
 
-JNIEXPORT void JNICALL Java_com_artcom_mobile_BaseNativeLib_onFrame(JNIEnv * env, jobject obj) {
+JNIEXPORT void JNICALL Java_com_artcom_mobile_Base_NativeBinding_onFrame(JNIEnv * env, jobject obj) {
     ourApp.onFrame();
 }
 
-JNIEXPORT void JNICALL Java_com_artcom_mobile_BaseNativeLib_onTouch(JNIEnv * env, jobject obj) {
+JNIEXPORT void JNICALL Java_com_artcom_mobile_Base_NativeBinding_onTouch(JNIEnv * env, jobject obj) {
     ourApp.onTouch();
 }
 #endif
