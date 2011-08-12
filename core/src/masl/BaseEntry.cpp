a6c2d78454686fc8ae9d0d55cdaa76cc78004bbc
valentin <valentin.schunack@artcom.de>
2011-08-12 14:01:14 +0200

    (SparkMobile) some refactoring

--- core/src/masl/BaseEntry.cpp
+++ core/src/masl/BaseEntry.cpp
@@ -18,28 +18,28 @@ namespace masl {
 #ifdef __ANDROID__
 /////////////////////////////////////////////////////////////////////////JNI
 extern "C" {
-    JNIEXPORT bool JNICALL Java_com_artcom_mobile_BaseBinding_loadSpark(JNIEnv * env, jobject obj,
+    JNIEXPORT bool JNICALL Java_com_artcom_mobile_Base_NativeBinding_loadSpark(JNIEnv * env, jobject obj,
                                                                           jstring filename);
-    JNIEXPORT void JNICALL Java_com_artcom_mobile_BaseBinding_log(JNIEnv * env, jobject obj,
+    JNIEXPORT void JNICALL Java_com_artcom_mobile_Base_NativeBinding_log(JNIEnv * env, jobject obj,
                                                                   int theSeverity, jstring theFilename, 
                                                                   int theLineNumber, jstring theMessage);
                                                                           
 };
 
-JNIEXPORT bool JNICALL Java_com_artcom_mobile_BaseBinding_loadSpark(JNIEnv * env, jobject obj,
+JNIEXPORT bool JNICALL Java_com_artcom_mobile_Base_NativeBinding_loadSpark(JNIEnv * env, jobject obj,
                                                                  jstring filename) {
     jboolean isCopy;
     const char* str = env->GetStringUTFChars(filename, &isCopy);                                                                    
     return masl::loadSpark(str);    
 }
 
-JNIEXPORT void JNICALL Java_com_artcom_mobile_BaseBinding_log(JNIEnv * env, jobject obj,
+JNIEXPORT void JNICALL Java_com_artcom_mobile_Base_NativeBinding_log(JNIEnv * env, jobject obj,
                                                               int theSeverity, jstring theFilename, 
                                                               int theLineNumber, jstring theMessage) {
     jboolean isCopy;
     const char* myMessage = env->GetStringUTFChars(theMessage, &isCopy);                                                                    
     const char* myFilename = env->GetStringUTFChars(theFilename, &isCopy);                                                                    
-    asl::Logger::get().log(/*myTime,*/ asl::SEV_PRINT, myFilename, theLineNumber, myMessage);
+    masl::Logger::get().log(/*myTime,*/ masl::SEV_PRINT, myFilename, theLineNumber, myMessage);
 }
 
 #endif
--- core/src/masl/Tests/TestMasl.cpp
+++ core/src/masl/Tests/TestMasl.cpp
@@ -23,13 +23,8 @@ extern "C" {
     JNIEXPORT jstring JNICALL Java_com_artcom_mobile_test_TestBaseBinding_test(JNIEnv * env, jobject obj);
 };
 
-<<<<<<< HEAD:core/src/base/Tests/TestBase.cpp
 JNIEXPORT jstring JNICALL Java_com_artcom_mobile_test_TestBaseBinding_test(JNIEnv * env, jobject obj) {
-    asl::MyBaseTestSuite mySuite("BaseTest", 0, 0);
-=======
-JNIEXPORT jstring JNICALL Java_com_artcom_mobile_test_TestBaseLib_test(JNIEnv * env, jobject obj) {
     masl::MyBaseTestSuite mySuite("BaseTest", 0, 0);
->>>>>>> c2e9dd77ce181ac944dd3fa38ec69ec0a3419a99:core/src/masl/Tests/TestMasl.cpp
     mySuite.run();
     return (env->NewStringUTF(mySuite.getFailedTestMessages().c_str()));
     
