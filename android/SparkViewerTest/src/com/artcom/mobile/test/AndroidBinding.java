0393e393d5a4ee2c838de224ce39cef07dd2808b
valentin <valentin.schunack@artcom.de>
2011-08-15 18:53:46 +0200

    (Sparkmobile) cleanup repo

---
+++ android/SparkViewerTest/src/com/artcom/mobile/test/AndroidBinding.java
@@ -0,0 +1,14 @@
+package com.artcom.mobile.test;
+
+import android.util.Log;
+
+public class AndroidBinding {
+
+    static {
+        System.loadLibrary("masl");
+        System.loadLibrary("android"); 
+        System.loadLibrary("testAndroid");
+    }
+
+    public static native String test(String apkPath);
+}
