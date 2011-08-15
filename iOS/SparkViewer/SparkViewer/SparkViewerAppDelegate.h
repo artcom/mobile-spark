3433a958b73d9c5c4443360b8f0317e7eed79247
Arvid Hansen Diaz <arvid@monedula.intern.artcom.de>
2011-08-15 19:01:33 +0200

    starting basic iOS Application

---
+++ iOS/SparkViewer/SparkViewer/SparkViewerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  SparkViewerAppDelegate.h
+//  SparkViewer
+//
+//  Created by Arvid Hansen Diaz on 15.08.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SparkViewerAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
