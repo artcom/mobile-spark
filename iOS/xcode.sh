fcae4ef468a79daf0c0ac54e097a694807ce4b8b
Arvid Hansen Diaz <arvid@monedula.intern.artcom.de>
2011-08-15 19:02:22 +0200

    added MACOSX_DEPLOYMENT_TARGET to xcode build script

---
+++ iOS/xcode.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+
+export IS_TARGET_PLATFORM_IOS="TRUE"
+#export IOS_PLATFORM="iPhoneOS"
+export IOS_PLATFORM="iPhoneSimulator"
+export MACOSX_DEPLOYMENT_TARGET="Defeault Compiler"
+cmake -GXcode -DCMAKE_TOOLCHAIN_FILE=../../acmake/toolchain/iOS.toolchain.Xcode.cmake ../..
+#$MAKE_TOOL
