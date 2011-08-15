186bb03a21ab78a0886c700f019f5c998aba63e4
Arvid Hansen Diaz <arvid@monedula.intern.artcom.de>
2011-08-15 18:58:16 +0200

    clean up old unused ios files

---
+++ UiViewController.h
@@ -0,0 +1,16 @@
+//
+//  UiViewController.h
+//  mobile-spark
+//
+//  Created by Arvid Hansen Diaz on 15.08.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface UiViewController : UIViewController {
+    
+}
+
+@end
