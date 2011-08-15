38a4b62ffa112406ee44fa91171f6f38a9b6157f
Arvid Hansen Diaz <arvid@monedula.intern.artcom.de>
2011-08-15 19:01:33 +0200

    starting basic iOS Application

---
+++ iOS/SparkViewer/SparkViewer/GLView.h
@@ -0,0 +1,16 @@
+//
+//  GLView.h
+//  mobile-spark
+//
+//  Created by Arvid Hansen Diaz on 15.08.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface GLView : UIView {
+    
+}
+
+@end
---
+++ iOS/SparkViewer/SparkViewer/GLView.m
@@ -0,0 +1,37 @@
+//
+//  GLView.m
+//  mobile-spark
+//
+//  Created by Arvid Hansen Diaz on 15.08.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "GLView.h"
+
+
+@implementation GLView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
