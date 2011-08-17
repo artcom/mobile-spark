dcf4d6061cc90c3a42002a92d56e3c755c5932f4
Arvid Hansen Diaz <arvid@monedula.intern.artcom.de>
2011-08-17 17:34:41 +0200

    cleaning up unused files

--- iOS/xcode.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-
-export IS_TARGET_PLATFORM_IOS="TRUE"
-#export IOS_PLATFORM="iPhoneOS"
-export IOS_PLATFORM="iPhoneSimulator"
-export MACOSX_DEPLOYMENT_TARGET="Compiler Default"
-cmake -GXcode -DCMAKE_TOOLCHAIN_FILE=../../acmake/toolchain/iOS.toolchain.Xcode.cmake ../..
-#$MAKE_TOOL
