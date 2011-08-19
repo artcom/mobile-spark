a1035f2ee6a23d0c1b6d835f212347f408a1aa35
katjae <katjae@artcom.de>
2011-08-19 10:48:46 +0200

    added delay animation

---
+++ core/src/animation/DelayAnimation.cpp
@@ -0,0 +1,9 @@
+#include "DelayAnimation.h"
+
+namespace animation {
+    DelayAnimation::DelayAnimation(const long theDuration) : Animation(theDuration) {
+    }
+
+    DelayAnimation::~DelayAnimation() {
+    }
+}
---
+++ core/src/animation/DelayAnimation.h
@@ -0,0 +1,16 @@
+#ifndef _ac_mobile_animation_DelayAnimation_h_included_
+#define _ac_mobile_animation_DelayAnimation_h_included_
+
+#include "Animation.h"
+
+namespace animation {
+    class DelayAnimation : public Animation {
+    public:
+        DelayAnimation(const long theDuration=1000);
+        virtual ~DelayAnimation();
+    };
+
+    typedef boost::shared_ptr<DelayAnimation> DelayAnimationPtr;
+};
+
+#endif
