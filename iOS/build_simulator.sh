a6c63bb09666159d4595ee205a7fe26f15447664
gunnar <gunnar@artcom.de>
2011-08-23 13:19:27 +0200

    refactored cmake to use module ac_add_library

--- iOS/build_simulator.sh
+++ iOS/build_simulator.sh
@@ -2,9 +2,6 @@
 
 cd ../_iOS_build
 
-export IS_TARGET_PLATFORM_IOS="TRUE"
-export IOS_PLATFORM="iPhoneSimulator"
-
 cmake -DCMAKE_TOOLCHAIN_FILE=../acmake/toolchains/ios.toolchain.Simulator.cmake ..
 #$MAKE_TOOL
 
