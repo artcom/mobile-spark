937d6d905a366f532d249227e7c75fe1f6675bbf
katjae <katjae@artcom.de>
2011-08-31 16:12:47 +0200

    push objs to sdcard to reduce building time

--- android/push.sh
+++ android/push.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-adb push ./SparkViewer/assets/layouts/main.spark /sdcard/main.spark
+echo "push /SparkViewer/assets/layouts/main.spark"
+adb push ./SparkViewer/assets/layouts/main.spark /sdcard/
+
+PUSHPATH="./dataToPush/*"
+
+for f in $PUSHPATH
+do
+    echo "push $f"
+    adb push $f /sdcard/
+done
 
