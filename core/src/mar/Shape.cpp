270dadf5bd2727ed326a262f7a99bcb9f5461940
Arvid Hansen Diaz <arvid@monedula.intern.artcom.de>
2011-09-07 12:23:58 +0200

    added update VertexBuffers functionality added seperated Vertex Shaders for textured Objects and untextured Objects

--- core/src/mar/Shape.cpp
+++ core/src/mar/Shape.cpp
@@ -19,27 +19,9 @@ namespace mar {
                                                       it != elementList.end(); ++it) {
             ElementPtr element = *it;
             element->loadData(theMatrix);
-            
-            /*
-            element->material->loadShader(theMatrix);
-            glBindBuffer(GL_ARRAY_BUFFER, element->vertexBuffer);
-            glVertexAttribPointer(ATTRIB_VERTEX, 3, GL_FLOAT, GL_FALSE, VERTEX_SIZE, 0);        
-            glEnableVertexAttribArray(ATTRIB_VERTEX);  
-
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element->indexBuffer);
-            checkGlError("glDrawElements 0");
-
-            glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_SHORT, 0);
-            checkGlError("glDrawElements 1");
-
-            glBindBuffer(GL_ARRAY_BUFFER, 0);
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-            */
-            
             glDrawElements(GL_TRIANGLES, element->numVertices, GL_UNSIGNED_SHORT, 0);
             element->unloadData();
-            
-            
+             
             checkGlError("glDrawElements 2");
         }
     }
@@ -52,26 +34,11 @@ namespace mar {
             if (element && element->material ) {
                  element->material->initGL();
             }
-            //glBindAttribLocation(element->material->shaderProgram, ATTRIB_VERTEX, "a_position");
+
             glBindAttribLocation(element->material->shaderProgram, VERTEX_POS_INDEX, "a_position");
 
-            
-            AC_PRINT << "ATTRIB_VERTEX: " << ATTRIB_VERTEX;
-      
-            
             element->createVertexBuffers();
-            /*
-            glGenBuffers(1, &element->vertexBuffer);
-            glBindBuffer(GL_ARRAY_BUFFER, element->vertexBuffer);
-            glBufferData(GL_ARRAY_BUFFER, (sizeof(float) * 12), element->testVertices.get(), GL_STATIC_DRAW);
-            glBindBuffer(GL_ARRAY_BUFFER, 0);
-            
-            
-            glGenBuffers(1, &element->indexBuffer);
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element->indexBuffer);
-            glBufferData(GL_ELEMENT_ARRAY_BUFFER, (sizeof(GLushort) * 4), element->testIndices.get(), GL_STATIC_DRAW); 
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-             */
+
 
         }
     }
@@ -110,7 +77,7 @@ namespace mar {
         myElement->numVertices = 6;
         myElement->vertexData = boost::shared_array<float>(new float[(myElement->numVertices) * _myDataPerVertex]);
         myElement->indexDataVBO = boost::shared_array<GLushort>(new GLushort[myElement->numVertices]);
-        int myXYCoords[] = {0.0f, 0.0f, 50.0f, 0.0f, 0.0f, 50.0f, 50.0f, 0.0f, 50.0f, 50.0f, 0.0f, 50.0f};
+        int myXYCoords[] = {0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f};
 
         for (size_t i = 0, l = myElement->numVertices; i < l; ++i) {
             (myElement->vertexData)[i * _myDataPerVertex + 0] = myXYCoords[i * 2 + 0];
@@ -136,6 +103,7 @@ namespace mar {
         myElement->vertexData[_myDataPerVertex*5 + 1] = theHeight;
         _myBoundingBox.max[0] = theWidth;
         _myBoundingBox.max[1] = theHeight;
+        myElement->updateCompleteVertexBuffersContent();
         AC_PRINT << "setDimensions " << theWidth << ", " << theHeight << " " << _myBoundingBox.min << ", " << _myBoundingBox.max;
     }
 
