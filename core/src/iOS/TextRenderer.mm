a0f4044407de4ae0bd6150a27f3cd4d278f2b8b7
Arvid Hansen Diaz <arvid@monedula.intern.artcom.de>
2011-09-12 13:08:23 +0200

    added TextRenderer header and class

---
+++ core/src/iOS/TextRenderer.mm
@@ -0,0 +1,16 @@
+#import "TextRenderer.h"
+
+namespace ios {
+    TextRenderer::TextRenderer()
+    {
+    }
+
+    TextRenderer::~TextRenderer()
+    {
+    }
+    
+    void TextRenderer::renderText(const std::string & theMessage, int theTextureId, int theFontSize, vector4 theColor)
+    {
+        
+    }
+}
