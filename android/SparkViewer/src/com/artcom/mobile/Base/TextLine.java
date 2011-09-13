ec77f17d3dbad3bab683eddbb19038fa22ad343c
valentin <valentin.schunack@artcom.de>
2011-09-13 09:05:18 +0200

    (sm) added forgotten files

---
+++ android/SparkViewer/src/com/artcom/mobile/Base/TextLine.java
@@ -0,0 +1,15 @@
+package com.artcom.mobile.Base;
+
+import android.content.Context;
+
+public class TextLine {
+	public String _myLineOfText;
+	public int _myXPos;
+	public int _myYPos;
+    public TextLine(String myTextLine, int myXPos, int myYPos) {
+    	_myLineOfText = myTextLine;
+    	_myXPos = myXPos;
+    	_myYPos = myYPos;
+    }
+	
+}
