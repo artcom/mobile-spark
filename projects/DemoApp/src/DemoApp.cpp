0f202c33435af1b9a057f2e0db5482dc04276306
Manuel Kloess <manuel.kloess@artcom.de>
2011-09-14 19:07:12 +0200

    android 3d-view like window

--- projects/DemoApp/src/DemoApp.cpp
+++ projects/DemoApp/src/DemoApp.cpp
@@ -253,14 +253,13 @@ namespace demoapp {
 	void DemoApp::onSensorEvent(EventPtr theEvent) {
 		if (_mySlides[_myCurrentSlide]->getName() != "3D-Viewer-Slide") return;
     	SensorEventPtr myEvent = boost::static_pointer_cast<SensorEvent>(theEvent);
-    	//float myX = myEvent->getValue0(); 
-    	float myY = myEvent->getValue1(); 
-    	float myZ = myEvent->getValue2();
+    	float myX = myEvent->getValue0()*3.1417/180; 
+    	float myY = myEvent->getValue1()*3.1417/180; 
+    	//float myZ = myEvent->getValue2()*3.1417/180;
     	ComponentPtr myAmazoneObject = _mySparkWindow->getChildByName("3dworld")->getChildByName("transform")->getChildByName("theAmazone");
         Shape3DPtr myAmazone = boost::static_pointer_cast<spark::Shape3D>(myAmazoneObject);
-        myAmazone -> setRotationY(myAmazone->getRotationY() - myZ/180);
-		myAmazone -> setRotationX(myAmazone->getRotationX() - myY/180 - 0.2);
-
+		myAmazone -> setRotationX(-myY - 1.57);
+		myAmazone -> setRotationY(-myX * 2); // x2 for more effect
     }
 
 	void DemoApp::onSensorLightEvent(EventPtr theEvent) {
