af95e7c81c30bb5be4e1ffdae96e74e87a932176
katjae <katjae@artcom.de>
2011-09-16 12:29:35 +0200

    renamings in demoapp

--- projects/DemoApp/android/build.sh
+++ projects/DemoApp/android/build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-JAVA_ACTIVITY="DemoAppActivity" LIB_ACTIVITY="demoapp" ../../../android/build_project.sh
+PROJECT_NAME="DemoApp"  ../../../android/build_project.sh
--- projects/DemoApp/android/push.sh
+++ projects/DemoApp/android/push.sh
@@ -2,7 +2,7 @@
 
 cd ..
 
-APP_NAME="demoapp" ../../android/push_project.sh $*
+PROJECT_NAME="DemoApp" ../../android/push_project.sh $*
 
 cd -
 
