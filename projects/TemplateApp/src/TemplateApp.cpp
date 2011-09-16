15ada094b11d6d42f511a28124df8546d144b776
Manuel Kloess <manuel.kloess@artcom.de>
2011-09-16 10:47:27 +0200

    empty template for new android projects

---
+++ projects/TemplateApp/src/TemplateApp.cpp
@@ -0,0 +1,48 @@
+#include "TemplateApp.h"
+#include <cstdlib>
+
+#include <boost/smart_ptr/shared_ptr.hpp>
+
+#include <masl/Logger.h>
+#include <masl/MobileSDK_Singleton.h>
+
+#include <spark/Event.h>
+#include <spark/SparkComponentFactory.h>
+#include <spark/AppProvider.h>
+
+
+using namespace spark;
+using namespace masl;
+
+/////////////////////////////////////////////////////////////////////////App-Instance
+#ifdef __ANDROID__
+JNIEXPORT jint JNICALL
+JNI_OnLoad(JavaVM *vm, void *reserved) {
+    spark::AppProvider::get().setApp(boost::shared_ptr<templateapp::TemplateApp>(new templateapp::TemplateApp()));
+    return JNI_VERSION_1_6;
+}
+#endif
+
+
+/////////////////// Application code, this should be in java or script language later...
+namespace templateapp {
+
+	
+   
+    TemplateApp::TemplateApp():BaseApp("templateapp") {
+    }
+
+    TemplateApp::~TemplateApp() {
+    }
+
+    void TemplateApp::setup(const masl::UInt64 theCurrentMillis, const std::string & theAssetPath, int theScreenWidth, int theScreenHeight) {
+        BaseApp::setup(theCurrentMillis, theAssetPath, theScreenWidth, theScreenHeight);
+        std::string myOrientation;
+        std::string mySparkFile = findBestMatchedLayout("/main", theScreenWidth, theScreenHeight, myOrientation);
+        MobileSDK_Singleton::get().freezeMobileOrientation(myOrientation);
+        loadLayoutAndRegisterEvents(mySparkFile);
+    }
+
+}
+
+
---
+++ projects/TemplateApp/src/TemplateApp.h
@@ -0,0 +1,30 @@
+#ifndef _included_mobile_templateapp_TemplateApp_
+#define _included_mobile_templateapp_TemplateApp_
+
+/////////////////// Application code, this should be in java or script language later...
+#include <spark/BaseApp.h>
+
+#ifdef __ANDROID__
+extern "C" {
+    JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved);
+}
+#endif
+
+namespace templateapp {
+    class TemplateApp: public spark::BaseApp {
+        public: 
+            TemplateApp();
+            virtual ~TemplateApp();
+            virtual void setup(const masl::UInt64 theCurrentMillis, const std::string & theAssetPath, int theScreenWidth, int theScreenHeight);
+
+        private:
+        	
+    };
+
+    typedef boost::shared_ptr<TemplateApp> TemplateAppPtr;
+    typedef spark::MemberFunctionEventCallback<TemplateApp, TemplateAppPtr> TemplateEventCB;
+
+};
+
+#endif 
+
