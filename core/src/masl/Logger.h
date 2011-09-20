b1a939100dc22cc1b3657c7d37ccbbce777e3d1b
gunnar <gunnar@artcom.de>
2011-09-20 18:45:58 +0200

    added module verbosity

--- core/src/masl/Logger.h
+++ core/src/masl/Logger.h
@@ -9,20 +9,61 @@
 namespace masl {
     enum Severity {SEV_PRINT = 0, SEV_TESTRESULT, SEV_FATAL, SEV_ERROR, SEV_WARNING, SEV_INFO, SEV_DEBUG, SEV_TRACE, SEV_DISABLED};
 
+    /**
+     * Reads the environment variables 'AC_LOG_VERBOSITY' and 'AC_MODULE_VERBOSITY'
+     * to initialize the global logging verbosity and the per-module verbosities.
+     *
+     * Per-module verbosity has the following format:
+     * AC_MODULE_VERBOSITY := MODULE_VERBOSITY | AC_MODULE_VERBOSITY:MODULE_VERBOSITY
+     * MODULE_VERBOSITY := <LEVEL>/<MODULE>[/<FROMLINE>[/<TOLINE>]]
+     */
     class Logger : public Singleton<Logger> {
+        struct ModuleSeverity {
+            ModuleSeverity() {}
+            ModuleSeverity(Severity theSeverity,int theMinId, int myMaxId)
+                : mySeverity(theSeverity), myMinId(theMinId), myMaxId(myMaxId)
+            {}
+
+            Severity mySeverity;
+            int myMinId;
+            int myMaxId;
+        };
         public:
             Logger();
             virtual ~Logger();
 
-            inline bool ifLog(/*masl::Time theTime,*/ Severity theSeverity, const char * theModule, int theId) {
+            /**
+            Used to detect if a message should be logged depending on its severity and the logger severity settings.
+            returns true if theSeverity is higher (numerically smaller) than the verbosity setting
+            a different verbosity can be defined for any id range in any module; if there are different
+            verbosity settings for an overlapping id region in the same module, the setting for the
+            smallest id-range takes precedence.
+            */
+            bool ifLog(Severity theSeverity, const char * theModule, int theId);
+            /*inline bool ifLog(Severity theSeverity, const char * theModule, int theId) {
                 return theSeverity <= _myGlobalSeverity;
-            }
+            }*/
             void log(/*masl::Time theTime,*/ Severity theSeverity, const char * theModule, int theId, const std::string & theText);
             void setLoggerTopLevelTag(const std::string & theTagString);
             void setSeverity(const Severity theSeverity) { _myGlobalSeverity = theSeverity;};
+            /**
+            sets the verbosity for a particular module; an id range can be supplied;
+            the default settings will be that all module ids (line numbers) in a range will be selected.
+            */
+            void setModuleSeverity(Severity theSeverity,
+                const std::string & theModule,
+                int theMidId = 0,
+                int theMaxId = std::numeric_limits<int>::max() );
+    
+            /** sets the verbosity for a particular module; an id range can be supplied;
+                the default settings will be that all module ids (line numbers) in a range will be selected. */
+            void setModuleSeverity(const std::string & theSeverity);
+
         private:
+            Severity getSeverityFromString(std::string theString, Severity theDefault) const;
             std::string _myTopLevelLogTag;
             Severity _myGlobalSeverity;
+            std::multimap<std::string,ModuleSeverity> _mySeveritySettings;
     };
 
     /**
