adb97e8ff7065cf459af0136ba5bb3b73d1bb0d5
Manuel Kloess <manuel.kloess@artcom.de>
2011-09-21 10:27:21 +0200

    update ACProjectView

--- projects/ACProjectView/src/ACProjectView.cpp
+++ projects/ACProjectView/src/ACProjectView.cpp
@@ -50,15 +50,24 @@ namespace acprojectview {
         loadLayoutAndRegisterEvents(mySparkFile);
         BaseApp::realize();
         
+        
         ACProjectViewPtr ptr = boost::static_pointer_cast<ACProjectView>(shared_from_this());
         _myProjectMenu =  boost::static_pointer_cast<ProjectMenu>(_mySparkWindow->getChildByName("2dworld")->getChildByName("main",true));
         _myProjectViewer = boost::static_pointer_cast<ProjectViewerImpl>(_mySparkWindow->getChildByName("2dworld")->getChildByName("projectViewer",true));
 
+        _myWidth = _myProjectMenu->getPreviewWidth();
+        _myHeight = _myProjectMenu->getPreviewHeight();
+        _myProjectViewer->setWidth(_myWidth);
+        _myProjectViewer->setWidth(_myHeight);
         
         spark::EventCallbackPtr myPickedCB = EventCallbackPtr(new ACProjectViewEventCB(ptr, &ACProjectView::onProjectItem));
         spark::EventCallbackPtr myBackCB = EventCallbackPtr(new ACProjectViewEventCB(ptr, &ACProjectView::onBack));
-        
+        spark::EventCallbackPtr mySwipeCB = EventCallbackPtr(new ACProjectViewEventCB(ptr, &ACProjectView::onSwipeCB));
+
         _myProjectViewer->addEventListener(TouchEvent::PICKED, myBackCB,true);
+        _mySparkWindow->addEventListener(GestureEvent::SWIPE_LEFT, mySwipeCB);
+        _mySparkWindow->addEventListener(GestureEvent::SWIPE_RIGHT, mySwipeCB);
+        
         
         _myProjectItems = boost::static_pointer_cast<spark::Container>(_myProjectMenu);
         const VectorOfComponentPtr & myChildren = _myProjectItems->getChildrenByType(ProjectImpl::SPARK_TYPE);
@@ -72,35 +81,48 @@ namespace acprojectview {
     }
     
     void ACProjectView::onProjectItem(EventPtr theEvent) {
-        AC_PRINT << ":) __________ITEM: " << theEvent->getTarget()->getParent()->getName();
         _myProjectViewer->setVisible(true);
         _myProjectViewer->setSensible(true);
         _myProjectMenu->setSensible(false);
         _myCurrentProject = boost::static_pointer_cast<ProjectImpl>(theEvent->getTarget()->getParent());
-        projectViewAnimation(_myCurrentProject->getX()+_myProjectMenu->getPreviewWidth()/2, 0, _myCurrentProject->getY() + _myProjectMenu->getPreviewHeight()/2, 0, 0, 1);
-         _myProjectViewer->showProject(_myCurrentProject);
+        _myProjectViewer->showProject(_myCurrentProject);
+        projectViewAnimation(true);
     }
 
+    
     void ACProjectView::onBack(EventPtr theEvent) {
             AC_PRINT<< "----------- BACK";
-        projectViewAnimation(0, _myCurrentProject->getX() + _myProjectMenu->getPreviewWidth()/2, 0, _myCurrentProject->getY() + _myProjectMenu->getPreviewHeight()/2, 1, 0);
+        projectViewAnimation(false);
         _myProjectMenu->setSensible(true);
         _myProjectViewer->setSensible(false);
     }
     
-    void ACProjectView::projectViewAnimation(int fromX,int toX,int fromY,int toY,int fromScale,int toScale){
+    void ACProjectView::onSwipeCB(EventPtr theEvent) {
+        _myProjectViewer->onSwipe(theEvent);
+    }
+
+    
+    //void ACProjectView::projectViewAnimation(int fromX,int toX,int fromY,int toY,int fromScale,int toScale){
+    void ACProjectView::projectViewAnimation(bool showProject){
+        int toX = showProject ? 0 : _myCurrentProject->getX()+_myWidth/2;
+        int fromX   = showProject ? _myCurrentProject->getX()+_myWidth/2 : 0;
+        int toY = showProject ? 0 : _myCurrentProject->getY()+_myHeight/2;
+        int fromY   = showProject ? _myCurrentProject->getY()+_myHeight/2 : 0;
+        int toScale = showProject ? 1 :  0;
+        int fromScale   = showProject ? 0 :  1;
+        
         WidgetPropertyAnimationPtr myZoomAnimationX = WidgetPropertyAnimationPtr(
                 new WidgetPropertyAnimation(_myProjectViewer, &Widget::setScaleX, fromScale, toScale, _myAnimationTime,
-                    animation::EasingFnc(animation::linearTween)));
+                    animation::EasingFnc(animation::easeInOutQuad)));
         WidgetPropertyAnimationPtr myZoomAnimationY = WidgetPropertyAnimationPtr(
                 new WidgetPropertyAnimation(_myProjectViewer, &Widget::setScaleY, fromScale, toScale, _myAnimationTime,
-                    animation::EasingFnc(animation::linearTween)));
+                    animation::EasingFnc(animation::easeInOutQuad)));
         WidgetPropertyAnimationPtr myTransAnimationX = WidgetPropertyAnimationPtr(
                 new WidgetPropertyAnimation(_myProjectViewer, &Widget::setX, fromX, toX, _myAnimationTime,
-                    animation::EasingFnc(animation::linearTween)));
+                    animation::EasingFnc(animation::easeInOutQuad)));
         WidgetPropertyAnimationPtr myTransAnimationY = WidgetPropertyAnimationPtr(
                 new WidgetPropertyAnimation(_myProjectViewer, &Widget::setY, fromY, toY, _myAnimationTime,
-                    animation::EasingFnc(animation::linearTween)));
+                    animation::EasingFnc(animation::easeInOutQuad)));
         animation::ParallelAnimationPtr myParallel = animation::ParallelAnimationPtr(new animation::ParallelAnimation());
         myParallel->add(myZoomAnimationX);
         myParallel->add(myZoomAnimationY);
