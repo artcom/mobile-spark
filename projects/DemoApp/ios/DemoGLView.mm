a8c32eb18370065465693d0866dfee5d7a98d94f
Arvid Hansen Diaz <arvid@monedula.intern.artcom.de>
2011-09-27 11:27:13 +0200

    added, modified and corrected some comments

--- projects/DemoApp/ios/DemoGLView.mm
+++ projects/DemoApp/ios/DemoGLView.mm
@@ -7,7 +7,12 @@
 - (void) createApp
 {
     myApp = new demoapp::DemoApp();
+    
+    //activate Multisample anti aliasing with useMSAA2x, useMSAA4x, useMSAA8x
     MSAAQuality = useNoneMSAA;
+    
+    //choose framerate. Default is 1 -> 60hz. 2 -> 30 hz. Evertything < 1 is undefined.  
+    //frameInterval = 1;
 }
 
 - (void)dealloc
