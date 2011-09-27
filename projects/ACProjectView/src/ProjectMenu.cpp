f6685a689b7492b6efb5426a634a4ab511aaef28
Manuel Kloess <manuel.kloess@artcom.de>
2011-09-27 11:37:26 +0200

    ACProjectView: menu slide

--- projects/ACProjectView/src/ProjectMenu.cpp
+++ projects/ACProjectView/src/ProjectMenu.cpp
@@ -1,3 +1,9 @@
+#include <animation/AnimationManager.h>
+#include <animation/ParallelAnimation.h>
+#include <animation/SequenceAnimation.h>
+#include <animation/DelayAnimation.h>
+#include <animation/Easing.h>
+
 #include "spark/Image.h"
 #include "ProjectMenu.h"
 
@@ -15,10 +21,6 @@ namespace acprojectview {
         _myVerticalTiling = theXMLNode->getAttributeAs<int>("y-tiling",0); 
         _myGapX = theXMLNode->getAttributeAs<int>("x-gap",0); 
         _myGapY = theXMLNode->getAttributeAs<int>("y-gap",0);
-
-        
-
-        
     }
     
     void
@@ -26,29 +28,28 @@ namespace acprojectview {
         Transform::realize();
         _myWindowPtr = boost::static_pointer_cast<Window>(getRoot());
         ProjectMenuPtr myPtr = boost::static_pointer_cast<ProjectMenu>(shared_from_this());
-        spark::EventCallbackPtr dragCB = EventCallbackPtr(new ProjectMenuEventCB(myPtr, &ProjectMenu::onDragCB));
-        _myWindowPtr->addEventListener(GestureEvent::PAN, dragCB);
-        spark::EventCallbackPtr fingerDownCB = EventCallbackPtr(new ProjectMenuEventCB(myPtr, &ProjectMenu::onFingerDownCB));
-        _myWindowPtr->addEventListener(TouchEvent::DOWN, fingerDownCB);
-        
+        spark::EventCallbackPtr swipeLeftCB = EventCallbackPtr(new ProjectMenuEventCB(myPtr, &ProjectMenu::onSwipeLeftCB));
+        _myWindowPtr->addEventListener(GestureEvent::SWIPE_LEFT, swipeLeftCB);
+        spark::EventCallbackPtr swipeRightCB = EventCallbackPtr(new ProjectMenuEventCB(myPtr, &ProjectMenu::onSwipeRightCB));
+        _myWindowPtr->addEventListener(GestureEvent::SWIPE_RIGHT, swipeRightCB);
     }
 
     ProjectMenu::~ProjectMenu() {}
 
     void
     ProjectMenu::arrangeProjects() {
-
+        _myIsAnimating = false;      
         ProjectMenuPtr myPtr = boost::static_pointer_cast<ProjectMenu>(shared_from_this());
-
-        int theWidth = _myWindowPtr->getSize()[0];
-        int theHeight = _myWindowPtr->getSize()[1];
-        int dx = theWidth / _myHorizontalTiling;
-        int dy = theHeight / _myVerticalTiling;
+        _myCurrentSlide = 0;
+        myPtr->setX(0);
+        _myWidth = _myWindowPtr->getSize()[0];
+        _myHeight = _myWindowPtr->getSize()[1];
+        int dx = _myWidth / _myHorizontalTiling;
+        int dy = _myHeight / _myVerticalTiling;
         int iconWidth = dx - _myGapX;
         int iconHeight = dy - _myGapY;
-        //ProjectMenuPtr _myPtr = boost::static_pointer_cast<ProjectMenu>(shared_from_this());
-        
         const VectorOfComponentPtr & myChildren = myPtr->getChildrenByType(ProjectImpl::SPARK_TYPE);
+        _myNumberOfSlides = myChildren.size()/(_myHorizontalTiling * _myVerticalTiling);
         for (size_t i = 0; i < myChildren.size(); i++) {
             ProjectImplPtr myProject = boost::static_pointer_cast<ProjectImpl>(myChildren[i]);
             // set Position:
@@ -63,7 +64,6 @@ namespace acprojectview {
             image->setScaleX(scale);
             image->setScaleY(scale);
         }
-        
     }
     
     int ProjectMenu::getPreviewWidth() {
@@ -75,22 +75,40 @@ namespace acprojectview {
     }
     
     void
-    ProjectMenu::onDragCB(EventPtr theEvent) {
-        ProjectMenuPtr myPtr = boost::static_pointer_cast<ProjectMenu>(shared_from_this());
-        if(myPtr->getX() > 0) {
-            myPtr->setX(0);
-            return;
-        }
-        GestureEventPtr myEvent = boost::static_pointer_cast<GestureEvent>(theEvent);
-        myPtr->setX(_myStartDragX + myEvent->getTranslateX());
-        
-
+    ProjectMenu::onSwipeLeftCB(EventPtr theEvent) {
+        changeSlide(1);
     }
     
     void
-    ProjectMenu::onFingerDownCB(EventPtr theEvent) {
-        ProjectMenuPtr myPtr = boost::static_pointer_cast<ProjectMenu>(shared_from_this());
-        _myStartDragX = myPtr->getX();
+    ProjectMenu::onSwipeRightCB(EventPtr theEvent) {
+        changeSlide(-1);
     }
+    
+    void 
+    ProjectMenu::changeSlide(int dir) {
+        ProjectMenuPtr myPtr = boost::static_pointer_cast<ProjectMenu>(shared_from_this());
+        if(_myIsAnimating || _myCurrentSlide+dir < 0 || _myCurrentSlide+dir > _myNumberOfSlides) return;
+        _myIsAnimating = true;      
+        _myCurrentSlide += dir;
+        WidgetPropertyAnimationPtr changeAnimation = WidgetPropertyAnimationPtr(
+                new WidgetPropertyAnimation(myPtr, &Widget::setX, myPtr->getX(), myPtr->getX()-_myWidth*dir, 300,
+                    animation::EasingFnc(animation::easeInOutQuad)));
+         
+        animation::SequenceAnimationPtr mySeqAnimation = animation::SequenceAnimationPtr(new animation::SequenceAnimation());
+        animation::ParallelAnimationPtr myAnimation = animation::ParallelAnimationPtr(new animation::ParallelAnimation());
+        myAnimation->add(changeAnimation);
+        animation::DelayAnimationPtr myDelayAnim = animation::DelayAnimationPtr(new animation::DelayAnimation(0));
+        myDelayAnim->setOnFinish(masl::CallbackPtr(
+                        new masl::MemberFunctionCallback<ProjectMenu, ProjectMenuPtr>(myPtr, &ProjectMenu::onDelayFinished)));
+            
+        mySeqAnimation->add(myAnimation);
+        mySeqAnimation->add(myDelayAnim);
+        animation::AnimationManager::get().play(mySeqAnimation);
+    }
+    
+    void ProjectMenu::onDelayFinished() {        
+        _myIsAnimating = false;
+    }
+
     
 }
