b0b07f9de08c8663c78dd0353577bd0f9c97b910
gunnar <gunnar@artcom.de>
2011-10-06 16:40:21 +0200

    delete gl texture when deleting Texture

--- core/src/mar/Texture.cpp
+++ core/src/mar/Texture.cpp
@@ -1,14 +1,31 @@
 #include "Texture.h"
+#include <masl/Logger.h>
 
 namespace mar {
     Texture::Texture() :textureId_(0){
     }
 
-    GLuint Texture::getTextureId() const {
+    Texture::~Texture() {
+        AC_DEBUG << "delete Texture " << (void*)this;
+        unbind();
+    }
+
+    GLuint
+    Texture::getTextureId() const {
         return textureId_;
     }
-    void Texture::setTextureId(GLuint theTextureId){
+
+    void
+    Texture::setTextureId(GLuint theTextureId){
         textureId_ = theTextureId;
     }
 
+    void
+    Texture::unbind() {
+        if (textureId_) {
+            AC_DEBUG << "unbind gl texture " << textureId_;
+            glDeleteTextures(1, &textureId_);
+            textureId_ = 0;
+        }
+    }
 }
