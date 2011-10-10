4f655accfe870779262ad212eac96ee1d4dce427
Manuel Kloess <manuel.kloess@artcom.de>
2011-10-10 12:27:50 +0200

    ios text glyph index

--- core/src/iOS/TextRenderer.h
+++ core/src/iOS/TextRenderer.h
@@ -22,10 +22,14 @@ namespace ios {
         int getTextureID();
         int getTextureWidth();
         int getTextureHeight();
+        int getRenderedGlyphIndex();
     private:
+        int calcRenderedGlyphIndex(std::string theMessage, CFRange fitRange);
+
         GLuint  texture;
         int textureWidth;
         int textureHeight;
+        int renderedGlyphIndex;
         CGSize suggestedSize;
     };
     typedef masl::Ptr<TextRenderer> TextRendererPtr;
