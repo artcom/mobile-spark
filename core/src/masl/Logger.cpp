9cb2afdada530f6b857b24db30cbe83f5f61d35a
gunnar <gunnar@artcom.de>
2011-10-13 17:46:30 +0200

    AC_PRINT / dumpstack does not print fileline anymore

--- core/src/masl/Logger.cpp
+++ core/src/masl/Logger.cpp
@@ -103,40 +103,38 @@ namespace masl {
 
     void
     Logger::log(/*Time theTime, */ Severity theSeverity, const char * theModule, int theId, const std::string & theText) {
-        char buf[20];
-        sprintf(buf,"%i",theId);
         std::string myLogTag(_myTopLevelLogTag);
         if (theSeverity == SEV_TESTRESULT) {
             myLogTag += "/TestResult/";
         }
-        std::string myText(theText);
-        myText += " [";
-        myText += lastFileNamePart(theModule);
-        myText += " at:" + std::string(buf) + "]";
-
+        std::ostringstream myText;
+        myText << theText;
+        if (theSeverity > SEV_PRINT) {
+            myText << " [" << lastFileNamePart(theModule) << " at:" << theId << "]";
+        }
 
         #ifdef iOS
         switch (theSeverity) {
             case SEV_TRACE:
-                std::cout << myLogTag.c_str() << " TRACE: " << myText.c_str() << "\n";
+                std::cout << myLogTag << " TRACE: " << myText.str() << "\n";
                 break;
             case SEV_DEBUG:
-                std::cout << myLogTag.c_str() << " DEBUG: " << myText.c_str() << "\n";
+                std::cout << myLogTag << " DEBUG: " << myText.str() << "\n";
                 break;
             case SEV_INFO:
-                std::cout << myLogTag.c_str() << " INFO: " << myText.c_str() << "\n";
+                std::cout << myLogTag << " INFO: " << myText.str() << "\n";
                 break;
             case SEV_WARNING:
-                std::cout << myLogTag.c_str() << " WARNING: " << myText.c_str() << "\n";
+                std::cout << myLogTag << " WARNING: " << myText.str() << "\n";
                 break;
             case SEV_PRINT:
-                std::cout << myLogTag.c_str() << " LOG: " << myText.c_str() << "\n";
+                std::cout << myLogTag << " LOG: " << myText.str() << "\n";
                 break;
             case SEV_ERROR:
-                std::cout << myLogTag.c_str() << " ERROR: " << myText.c_str() << "\n";
+                std::cout << myLogTag << " ERROR: " << myText.str() << "\n";
                 break;
             case SEV_TESTRESULT :
-                std::cout << myLogTag.c_str() << " TEST: " << myText.c_str() << "\n";
+                std::cout << myLogTag << " TEST: " << myText.str() << "\n";
                 break;
             default:
                 throw Exception("Unknown logger severity");
@@ -146,23 +144,23 @@ namespace masl {
         #elif ANDROID
         switch (theSeverity) {
             case SEV_TRACE :
-                __android_log_print(ANDROID_LOG_VERBOSE, myLogTag.c_str(), myText.c_str());//__VA_ARGS__)
+                __android_log_print(ANDROID_LOG_VERBOSE, myLogTag.c_str(), myText.str().c_str());//__VA_ARGS__)
                 break;
             case SEV_DEBUG :
-                __android_log_print(ANDROID_LOG_DEBUG, myLogTag.c_str(), myText.c_str());//__VA_ARGS__)
+                __android_log_print(ANDROID_LOG_DEBUG, myLogTag.c_str(), myText.str().c_str());//__VA_ARGS__)
                 break;
             case SEV_WARNING :
-                __android_log_print(ANDROID_LOG_WARN, myLogTag.c_str(), myText.c_str());//__VA_ARGS__)
+                __android_log_print(ANDROID_LOG_WARN, myLogTag.c_str(), myText.str().c_str());//__VA_ARGS__)
                 break;
             case SEV_INFO :
             case SEV_PRINT :
-                __android_log_print(ANDROID_LOG_INFO, myLogTag.c_str(), myText.c_str());//__VA_ARGS__)
+                __android_log_print(ANDROID_LOG_INFO, myLogTag.c_str(), myText.str().c_str());//__VA_ARGS__)
                 break;
             case SEV_ERROR :
-                __android_log_print(ANDROID_LOG_ERROR, myLogTag.c_str(), myText.c_str());//__VA_ARGS__)
+                __android_log_print(ANDROID_LOG_ERROR, myLogTag.c_str(), myText.str().c_str());//__VA_ARGS__)
                 break;
             case SEV_TESTRESULT :
-                __android_log_print(ANDROID_LOG_INFO, myLogTag.c_str(), myText.c_str());//__VA_ARGS__)
+                __android_log_print(ANDROID_LOG_INFO, myLogTag.c_str(), myText.str().c_str());//__VA_ARGS__)
                 break;
             default:
                 throw Exception("Unknown logger severity");
