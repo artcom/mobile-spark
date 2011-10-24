847d045e39d0d25e0f03508dce3370b06bd02d9c
katjae <katjae@artcom.de>
2011-10-24 15:03:19 +0200

    licence

--- acmake/modules/FindBoost.cmake
+++ acmake/modules/FindBoost.cmake
@@ -1,2 +1,11 @@
+# __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
+#
+# Copyright (C) 1993-2011, ART+COM AG Berlin, Germany <www.artcom.de>
+#
+# It is distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+#  http://www.boost.org/LICENSE_1_0.txt)
+# __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
+
 find_path(Boost_INCLUDE_DIRS  boost/smart_ptr/shared_ptr.hpp HINTS ${core_SOURCE_DIR}/netsrc $ENV{BOOST_HOME} /opt/local/include NO_CMAKE_FIND_ROOT_PATH)
 
