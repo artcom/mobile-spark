4eec42a90bc9bb36fff410a1e1f7e82c76c9ee92
katjae <katjae@artcom.de>
2011-10-24 15:03:19 +0200

    licence

--- acmake/modules/FindlibXML2.cmake
+++ acmake/modules/FindlibXML2.cmake
@@ -1,3 +1,12 @@
+# __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
+#
+# Copyright (C) 1993-2011, ART+COM AG Berlin, Germany <www.artcom.de>
+#
+# It is distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+#  http://www.boost.org/LICENSE_1_0.txt)
+# __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
+
 if(ANDROID)
     find_path(libXML2_INCLUDE_DIR NAMES libxml/xpath.h PATHS ${core_SOURCE_DIR}/netsrc/libxml2/includes/ NO_CMAKE_FIND_ROOT_PATH)
     find_library(libXML2_LIBRARY_DIR libxml2.a ${core_SOURCE_DIR}/netsrc/libxml2/ NO_CMAKE_FIND_ROOT_PATH NO_CMAKE_SYSTEM_PATH NO_SYSTEM_ENVIRONMENT_PATH NO_DEFAULT_PATH )
