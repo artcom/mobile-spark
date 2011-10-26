e773880f91feaf5ae3445c3afab82812b82bf27d
katjae <katjae@artcom.de>
2011-10-26 11:14:49 +0200

    Element vertexData_ is not accessible from outside, set dirty flag when modified and call updateCompleteVertexBuffersContent from Element if necessary

--- core/src/mar/Shape.cpp
+++ core/src/mar/Shape.cpp
@@ -123,19 +123,19 @@ namespace mar {
         dataPerVertex_ = 3 + (myTexturedFlag ? 2 : 0);
         myElement->numVertices_ = 4;
         myElement->numIndices_ = 6;
-        myElement->vertexData_ = boost::shared_array<float>(new float[(myElement->numVertices_) * dataPerVertex_]);
+        myElement->setVertexData(boost::shared_array<float>(new float[(myElement->numVertices_) * dataPerVertex_]));
         GLushort indices[] = {0, 1, 2, 2, 1, 3};
         myElement->indexDataVBO_ = boost::shared_array<GLushort>(new GLushort[myElement->numIndices_]);
         float myXYCoords[] = {0.0f, 0.0f,1.0f, 0.0f, 0.0f, 1.0f,1.0f, 1.0f};
 
         for (size_t i = 0, l = myElement->numVertices_; i < l; ++i) {
-            (myElement->vertexData_)[i * dataPerVertex_ + 0] = myXYCoords[i * 2 + 0] * getWidth();
-            (myElement->vertexData_)[i * dataPerVertex_ + 1] = myXYCoords[i * 2 + 1] * getHeight();
-            (myElement->vertexData_)[i * dataPerVertex_ + 2] = 0;
+            (myElement->getVertexData())[i * dataPerVertex_ + 0] = myXYCoords[i * 2 + 0] * getWidth();
+            (myElement->getVertexData())[i * dataPerVertex_ + 1] = myXYCoords[i * 2 + 1] * getHeight();
+            (myElement->getVertexData())[i * dataPerVertex_ + 2] = 0;
 
             if (myTexturedFlag) {
-                (myElement->vertexData_)[i * dataPerVertex_ + 3] = myXYCoords[i * 2 + 0];
-                (myElement->vertexData_)[i * dataPerVertex_ + 4] = myXYCoords[i * 2 + 1];
+                (myElement->getVertexData())[i * dataPerVertex_ + 3] = myXYCoords[i * 2 + 0];
+                (myElement->getVertexData())[i * dataPerVertex_ + 4] = myXYCoords[i * 2 + 1];
             }
         }
 
@@ -148,17 +148,17 @@ namespace mar {
     RectangleShape::setDimensions(const vector2 & theLowerLeftCorner, const vector2 & theUpperRightCorner) {
         ElementPtr myElement = elementList_[0];
 
-        myElement->vertexData_[0] = theLowerLeftCorner[0];
-        myElement->vertexData_[0 + 1] = theLowerLeftCorner[1];
+        myElement->getVertexData()[0] = theLowerLeftCorner[0];
+        myElement->getVertexData()[0 + 1] = theLowerLeftCorner[1];
         
-        myElement->vertexData_[dataPerVertex_] = theUpperRightCorner[0];
-        myElement->vertexData_[dataPerVertex_ + 1] = theLowerLeftCorner[1];
+        myElement->getVertexData()[dataPerVertex_] = theUpperRightCorner[0];
+        myElement->getVertexData()[dataPerVertex_ + 1] = theLowerLeftCorner[1];
         
-        myElement->vertexData_[dataPerVertex_*2] = theLowerLeftCorner[0];
-        myElement->vertexData_[(dataPerVertex_*2) + 1] = theUpperRightCorner[1];
+        myElement->getVertexData()[dataPerVertex_*2] = theLowerLeftCorner[0];
+        myElement->getVertexData()[(dataPerVertex_*2) + 1] = theUpperRightCorner[1];
 
-        myElement->vertexData_[dataPerVertex_*3] = theUpperRightCorner[0];
-        myElement->vertexData_[(dataPerVertex_*3) + 1] =  theUpperRightCorner[1];
+        myElement->getVertexData()[dataPerVertex_*3] = theUpperRightCorner[0];
+        myElement->getVertexData()[(dataPerVertex_*3) + 1] =  theUpperRightCorner[1];
 
         upperRight_ = theUpperRightCorner;
         lowerLeft_ = theLowerLeftCorner;
@@ -167,25 +167,23 @@ namespace mar {
         boundingBox_.max[0] = theUpperRightCorner[0];
         boundingBox_.max[1] = theUpperRightCorner[1];
         
-        myElement->updateCompleteVertexBuffersContent();
     }
     
     void
     RectangleShape::setTexCoords(const vector2 & theUV0, const vector2 & theUV1, const vector2 & theUV2, const vector2 & theUV3) {
         ElementPtr myElement = elementList_[0];
         if (boost::dynamic_pointer_cast<ElementWithTexture>(myElement)) {
-            (myElement->vertexData_)[0 * dataPerVertex_ + 3] = theUV0[0];
-            (myElement->vertexData_)[0 * dataPerVertex_ + 4] = theUV0[1];
+            (myElement->getVertexData())[0 * dataPerVertex_ + 3] = theUV0[0];
+            (myElement->getVertexData())[0 * dataPerVertex_ + 4] = theUV0[1];
 
-            (myElement->vertexData_)[1 * dataPerVertex_ + 3] = theUV1[0];
-            (myElement->vertexData_)[1 * dataPerVertex_ + 4] = theUV1[1];
+            (myElement->getVertexData())[1 * dataPerVertex_ + 3] = theUV1[0];
+            (myElement->getVertexData())[1 * dataPerVertex_ + 4] = theUV1[1];
 
-            (myElement->vertexData_)[2 * dataPerVertex_ + 3] = theUV2[0];
-            (myElement->vertexData_)[2 * dataPerVertex_ + 4] = theUV2[1];
+            (myElement->getVertexData())[2 * dataPerVertex_ + 3] = theUV2[0];
+            (myElement->getVertexData())[2 * dataPerVertex_ + 4] = theUV2[1];
 
-            (myElement->vertexData_)[3 * dataPerVertex_ + 3] = theUV3[0];
-            (myElement->vertexData_)[3 * dataPerVertex_ + 4] = theUV3[1];
-            myElement->updateCompleteVertexBuffersContent();
+            (myElement->getVertexData())[3 * dataPerVertex_ + 3] = theUV3[0];
+            (myElement->getVertexData())[3 * dataPerVertex_ + 4] = theUV3[1];
         }
     }
 
@@ -219,7 +217,6 @@ namespace mar {
         edgeBottom_ = theBottomEdge;
         setVertexData();
         ElementPtr myElement = elementList_[0];
-        myElement->updateCompleteVertexBuffersContent();
     }
 
     void
@@ -229,7 +226,7 @@ namespace mar {
         size_t vertices_per_side = 4;
         myElement->numIndices_ = 54; //9 quads * 2 triangles per quad * 3 vertices per triangle
         myElement->numVertices_ = vertices_per_side * vertices_per_side;
-        myElement->vertexData_ = boost::shared_array<float>(new float[(myElement->numVertices_) * dataPerVertex_]);
+        myElement->setVertexData(boost::shared_array<float>(new float[(myElement->numVertices_) * dataPerVertex_]));
         myElement->indexDataVBO_ = boost::shared_array<GLushort>(new GLushort[(myElement->numIndices_)]);
         for (size_t i = 0, l = vertices_per_side; i < l; ++i) {
             for (size_t j = 0, m = vertices_per_side; j < m; ++j) {
@@ -263,11 +260,11 @@ namespace mar {
                 }
 
                 size_t v = i * vertices_per_side + j;
-                myElement->vertexData_[v * dataPerVertex_ + 0] = myX;
-                myElement->vertexData_[v * dataPerVertex_ + 1] = myY;
-                myElement->vertexData_[v * dataPerVertex_ + 2] = 0;
-                myElement->vertexData_[v * dataPerVertex_ + 3] = myS;
-                myElement->vertexData_[v * dataPerVertex_ + 4] = myT;
+                myElement->getVertexData()[v * dataPerVertex_ + 0] = myX;
+                myElement->getVertexData()[v * dataPerVertex_ + 1] = myY;
+                myElement->getVertexData()[v * dataPerVertex_ + 2] = 0;
+                myElement->getVertexData()[v * dataPerVertex_ + 3] = myS;
+                myElement->getVertexData()[v * dataPerVertex_ + 4] = myT;
             }
         }
         int indices[] = { 0, 1, 4, 4, 1, 5,
@@ -296,8 +293,6 @@ namespace mar {
 
         setVertexData();
         ElementPtr myElement = elementList_[0];        
-        myElement->updateCompleteVertexBuffersContent();
-        
     }
 
 
