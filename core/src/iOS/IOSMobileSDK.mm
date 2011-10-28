a7701e93869848610ae3cd780e8d864ca86cd49b
Manuel Kloess <manuel.kloess@artcom.de>
2011-10-28 14:26:38 +0200

    ios text update for header (glyph start index)

--- core/src/iOS/IOSMobileSDK.mm
+++ core/src/iOS/IOSMobileSDK.mm
@@ -30,7 +30,7 @@ namespace ios
         masl::TextInfo textInfo;        
         
         TextRendererPtr textRenderer = TextRendererPtr(new TextRenderer());
-        textRenderer.get()->renderText(theMessage.substr(theStartIndex), theTextureId, theFontSize, theColor, (float) theMaxWidth, (float) theMaxHeight, theAlign, theFontPath,theLineHeight);
+        textRenderer.get()->renderText(theMessage.substr(theStartIndex), theTextureId, theFontSize, theColor, (float) theMaxWidth, (float) theMaxHeight, theAlign, theFontPath,theLineHeight, theStartIndex);
         
         textInfo.textureID = textRenderer.get()->getTextureID();
         textInfo.height = textRenderer.get()->getTextureHeight();
