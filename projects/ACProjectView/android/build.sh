b61dd137922ec38ce4ca4c5b3082a7b90041d081
valentin <valentin.schunack@artcom.de>
2011-10-31 14:48:25 +0100

    (sm) made new assets path

--- projects/ACProjectView/android/build.sh
+++ projects/ACProjectView/android/build.sh
@@ -14,6 +14,7 @@ done
 
 if [ $DEPLOY == "1" ]
 then
+    mkdir $PROJECT_NAME/assets
     cp -ra ../assets/* $PROJECT_NAME/assets
 fi
 PROJECT_NAME="ACProjectView" ../../../android/build_project.sh $*
