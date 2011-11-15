49bc97ff7f0b5b60ceb52dbc0267b9279571b09b
katjae <katjae@artcom.de>
2011-11-15 18:34:01 +0100

    refactor SparkViewerActivity functionality to Delegate

--- android/SparkViewerBase/src/com/artcom/mobile/Base/SparkViewerActivity.java
+++ android/SparkViewerBase/src/com/artcom/mobile/Base/SparkViewerActivity.java
@@ -15,66 +15,32 @@ import com.artcom.mobile.Base.*;
 import android.content.pm.ActivityInfo;
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
-import com.artcom.mobile.Base.Sound;
-import com.artcom.mobile.Base.Music;
 
 public class SparkViewerActivity extends Activity {
 
+	protected String LOG_TAG = "SparkViewerActivity";
     private SparkOpenGLViewDelegate mViewDelegate;
-	private Bundle extras_;
-    private EnvMap envMap_;
-    private static String GLOBAL_VERBOSITY_ENV = "AC_LOG_VERBOSITY";
-    protected String LOG_TAG = "SparkViewerActivity";
-    protected String _myPackageName; //should be set by child classes
-    
-    SparkOpenGLView mView;
-    private EventManager eventManager;
-    private Sensors sensors;
-    private static Sound soundPlayer;
-    private static Music backgroundMusicPlayer;
-
+    protected String _myPackageName; //should be set by child classes  
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	mViewDelegate = new SparkOpenGLViewDelegate();
-    	NativeBinding.ourActivity = this;
+    	mViewDelegate = new SparkOpenGLViewDelegate(this);
+    	AudioDelegate audioDelegate = new AudioDelegate(this);
+    	
         AC_Log.setTopLevelTag(LOG_TAG);
         AC_Log.print("----------------------------------------------------------------------");
         AC_Log.print("-*************************SparkViewer********************************-");
         AC_Log.print("----------------------------------------------------------------------");
         super.onCreate(savedInstanceState);
-        extras_ = this.getIntent().getExtras();
-        envMap_ = new EnvMap();
-        envMap_.init(extras_);
 
-        CameraTexture.init(this);
-        eventManager = new EventManager();
-        sensors = new Sensors(this);
-        sensors.enable(Sensors.ORIENTATION);
-        sensors.enable(Sensors.LIGHT);
-        sensors.enable(Sensors.GYROSCOPE);
-        Severity mySeverity = envMap_.hasEnv(GLOBAL_VERBOSITY_ENV) ? Severity.fromString(envMap_.getEnv(GLOBAL_VERBOSITY_ENV)) : Severity.SEV_WARNING;
-        AC_Log.setSeverity(mySeverity);
-        AC_Log.print("severity: " + mySeverity);
-
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int myScreenWidth = dm.widthPixels;
-        int myScreenHeight = dm.heightPixels;
-
-        mView = new SparkOpenGLView(getApplication(), mViewDelegate, _myPackageName, myScreenWidth, myScreenHeight);
-        setContentView(mView);
-        
-        //init audio
-        soundPlayer = new Sound(this);
-        backgroundMusicPlayer = new Music(this);
+        mViewDelegate.init(this, _myPackageName);
+        setContentView(mViewDelegate.getView());
     }
 
     public boolean onTouchEvent(MotionEvent event) {
-        return eventManager.dumpTouchEvent(event) ;
+        return mViewDelegate.dumpTouchEvent(event) ;
     }
 
     @Override protected void onRestart() {
@@ -89,11 +55,9 @@ public class SparkViewerActivity extends Activity {
     @Override protected void onPause() {
         AC_Log.print("----------------------SparkViewer paused");
         super.onPause();
-        mView.onPause();
-        NativeBinding.onPause();
-        CameraTexture.closeCamera();
-        Sensors.disableAllSensors();
+        mViewDelegate.onPause();
     }
+
     @Override protected void onStop() {
         AC_Log.print("--------------------- on Stop");
         super.onStop();
@@ -102,7 +66,7 @@ public class SparkViewerActivity extends Activity {
     @Override protected void onResume() {
         AC_Log.print("------------------------SparkViewer resumed");
         super.onResume();
-        mView.onResume();
+        mViewDelegate.onResume();
     }
     
     @Override
@@ -111,7 +75,7 @@ public class SparkViewerActivity extends Activity {
     	if(event.getKeyCode() == KeyEvent.KEYCODE_BACK){
     		AC_Log.print("------------------------Backbutton pressed: "+keyCode);
 
-    		eventManager.dumbBackButtonEvent();
+    		mViewDelegate.dumbBackButtonEvent();
     		
     		return true;
     	}
@@ -119,51 +83,5 @@ public class SparkViewerActivity extends Activity {
    		return super.onKeyDown(keyCode, event);
 	}
 
-	///////////////////////////////static audio functions called from c++  
-    public static void playBackgroundMusic(String path, boolean isLoop){
-        backgroundMusicPlayer.playBackgroundMusic(path, isLoop);
-    }
-    public static void stopBackgroundMusic(){
-        backgroundMusicPlayer.stopBackgroundMusic();
-    }
-    public static void pauseBackgroundMusic(){
-        backgroundMusicPlayer.pauseBackgroundMusic();
-    }
-    public static void resumeBackgroundMusic(){
-        backgroundMusicPlayer.resumeBackgroundMusic();
-    }
-    public static void rewindBackgroundMusic(){
-        backgroundMusicPlayer.rewindBackgroundMusic();
-    }
-    public static boolean isBackgroundMusicPlaying(){
-        return backgroundMusicPlayer.isBackgroundMusicPlaying();
-    }
-    public static float getBackgroundMusicVolume(){
-        return backgroundMusicPlayer.getBackgroundVolume();
-    }
-    public static void setBackgroundMusicVolume(float volume){
-        backgroundMusicPlayer.setBackgroundVolume(volume);
-    }
-    public static int playEffect(String path){
-        return soundPlayer.playEffect(path);
-    }
-    public static void stopEffect(int soundId){
-        soundPlayer.stopEffect(soundId);
-    }
-    public static float getEffectsVolume(){
-        return soundPlayer.getEffectsVolume();
-    }
-    public static void setEffectsVolume(float volume){
-        soundPlayer.setEffectsVolume(volume);
-    }
-    public static void preloadEffect(String path){
-        soundPlayer.preloadEffect(path);
-    }
-    public static void unloadEffect(String path){
-        soundPlayer.unloadEffect(path);
-    }
-    public static void end(){
-        backgroundMusicPlayer.end();
-        soundPlayer.end();
-    }
+	
 }
