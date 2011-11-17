2ed5399311248bc684b5df258f04e96f7b28ddc3
valentin <valentin.schunack@artcom.de>
2011-11-17 12:27:22 +0100

    (sm) made it work again under windows/cygwin

--- projects/ACProjectView/android/build.sh
+++ projects/ACProjectView/android/build.sh
@@ -20,6 +20,10 @@ fi
 
 
 SPARK_COMPONENT_DIR=`pwd`/..
-cd $MOBILE_SPARK
+if [ "`uname -o`" == "Cygwin" ]; then
+    cd $(cygpath "$MOBILE_SPARK/")
+else
+    cd $MOBILE_SPARK
+fi    
 SPARK_COMPONENT_NAME="ACProjectView" SPARK_COMPONENT_DIR=$SPARK_COMPONENT_DIR android/build_project.sh $*
 cd $SPARK_COMPONENT_DIR/android
