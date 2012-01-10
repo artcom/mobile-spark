a77e2de9f2110d4c6deeb701f93c26cc2ac4b86d
Fabian <crocdialer@artcom.de>
2012-01-10 17:25:18 +0100

    still some work on ios buildscript

--- ios/build.sh
+++ ios/build.sh
@@ -20,14 +20,14 @@ mkdir "${OUT_DIR}/ios_Simulator/Debug"
 # change into buildDir
 cd ${BUILD_DIR}
 
-# build libraries for ios-DEVICE (armv7)
+# BUILD LIBRARIES for ios-DEVICE (armv7)
 cmake -GXcode -DCMAKE_TOOLCHAIN_FILE=${MYDIR}/../acmake/toolchains/ios.toolchain.iOS.Xcode.cmake ${MYDIR}/..
 
 xcodebuild -project ${BUILD_DIR}/mobile-spark.xcodeproj/ -target ALL_BUILD -configuration Debug
 mv ${MYDIR}/../_build/lib/*.a ${MYDIR}/../_build/lib/ios_Device/Debug
 
-xcodebuild -project ${BUILD_DIR}/mobile-spark.xcodeproj/ -target ALL_BUILD -configuration Release
-mv ${MYDIR}/../_build/lib/*.a ${MYDIR}/../_build/lib/ios_Device/Release
+#xcodebuild -project ${BUILD_DIR}/mobile-spark.xcodeproj/ -target ALL_BUILD -configuration Release
+#mv ${MYDIR}/../_build/lib/*.a ${MYDIR}/../_build/lib/ios_Device/Release
 
 rm -rf ${BUILD_DIR}/* 
 
@@ -38,8 +38,8 @@ cmake -GXcode -DCMAKE_TOOLCHAIN_FILE=${MYDIR}/../acmake/toolchains/ios.toolchain
 xcodebuild -project ${BUILD_DIR}/mobile-spark.xcodeproj/ -target ALL_BUILD -configuration Debug ARCHS=i386 ONLY_ACTIVE_ARCH=NO
 mv ${MYDIR}/../_build/lib/*.a ${MYDIR}/../_build/lib/ios_Simulator/Debug
 
-xcodebuild -project ${BUILD_DIR}/mobile-spark.xcodeproj/ -target ALL_BUILD -configuration Release ARCHS=i386 ONLY_ACTIVE_ARCH=NO
-mv ${MYDIR}/../_build/lib/*.a ${MYDIR}/../_build/lib/ios_Simulator/Release
+#xcodebuild -project ${BUILD_DIR}/mobile-spark.xcodeproj/ -target ALL_BUILD -configuration Release ARCHS=i386 ONLY_ACTIVE_ARCH=NO
+#mv ${MYDIR}/../_build/lib/*.a ${MYDIR}/../_build/lib/ios_Simulator/Release
 
 #rm -rf "${BUILD_DIR}/*"
 
@@ -51,5 +51,15 @@ for lib_i386_debug in `find ${OUT_DIR}/ios_Simulator/Debug -name "lib*\.a"`; do
 	lib_arm7_debug=`echo $lib_i386_debug | sed "s/ios_Simulator/ios_Device/g"`
     lib_arm7_release=`echo $lib_arm7_debug | sed "s/Debug/Release/g"`
     lib=`echo $lib_i386_debug | sed "s/ios_Simulator\/Debug\///g"`
-    lipo -arch armv7 $lib_arm7_release -arch i386 $lib_i386_release -create -output $lib
+    
+    lipoSucc=`lipo -arch armv7 $lib_arm7_debug -arch i386 $lib_i386_debug -create -output $lib`
+    #lipoSucc=`lipo -arch armv7 $lib_arm7_release -arch i386 $lib_i386_release -create -output $lib`
 done
+	echo "success: $lipoSucc"
+	
+# CLEAN UP
+
+rm -rf ${OUT_DIR}/ios_Simulator
+rm -rf ${OUT_DIR}/ios_Device
+
+
