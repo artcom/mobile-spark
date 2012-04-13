70f71916fcf99cadb4c24f4233ea28834ed2de39
Fabian <crocdialer@artcom.de>
2012-04-13 21:32:17 +0200

    fixed ios-buildscript. libxml now properly found again

--- acmake/toolchains/ios.toolchain.iOS.Xcode.cmake
+++ acmake/toolchains/ios.toolchain.iOS.Xcode.cmake
@@ -7,9 +7,12 @@
 #  http://www.boost.org/LICENSE_1_0.txt)
 # __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
 
+SET (CMAKE_CROSSCOMPILING 1)
+
 SET(TARGET_PLATFORM iPhoneOS)
 
-SET(IOS_DEPLOY_TGT "4.2")
+SET(IOS_SDK_VERSION "5.1")
+SET(IOS_DEPLOY_TGT "5.0")
 
 SET(IOS True)
 
@@ -30,7 +33,17 @@ ENDFOREACH(path)
 #Make sure SDK is valid
 find_path(SDKROOT "usr/include/stdlib.h" PATHS ${latest_SDK} NO_CMAKE_FIND_ROOT_PATH)
 
-SET(CMAKE_OSX_SYSROOT "${SDKROOT}")
+#SET(CMAKE_OSX_SYSROOT "${SDKROOT}")
+
+if(XCODE)
+    set(CMAKE_OSX_SYSROOT "iphoneos${IOS_SDK_VERSION}" CACHE STRING "SDK version" FORCE)
+else()
+    set(CMAKE_OSX_SYSROOT ${SDKROOT} CACHE STRING "SDK version" FORCE)
+endif()
+
+SET(CMAKE_CXX_COMPILER_WORKS TRUE)
+SET(CMAKE_C_COMPILER_WORKS TRUE)
+
 SET(CMAKE_XCODE_ATTRIBUTE_MACOSX_DEPLOYMENT_TARGET ${IOS_DEPLOY_TGT})
 
 #set target device: "1" -> iPhone, "2" -> iPad, "1,2 " -> both (remember the <whitespace> after the '2' !!!)
@@ -40,36 +53,37 @@ SET(CMAKE_SYSTEM_PROCESSOR arm)
 SET(CMAKE_OSX_ARCHITECTURES armv7)
 
 # Skip the platform compiler checks for cross compiling
-SET(CMAKE_CXX_COMPILER_WORKS TRUE)
-SET(CMAKE_C_COMPILER_WORKS TRUE)
 
 SET (CMAKE_C_COMPILER             "${DEVROOT}/usr/bin/clang")
-SET (CMAKE_C_FLAGS_DEBUG          "-g")
-SET (CMAKE_C_FLAGS_MINSIZEREL     "-Os -DNDEBUG")
-SET (CMAKE_C_FLAGS_RELEASE        "-O4 -DNDEBUG")
-SET (CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -g")
+SET (CMAKE_C_FLAGS_DEBUG          "-g -x objective-c")
+SET (CMAKE_C_FLAGS_MINSIZEREL     "-Os -DNDEBUG -x objective-c")
+SET (CMAKE_C_FLAGS_RELEASE        "-O4 -DNDEBUG -x objective-c")
+SET (CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -g -x objective-c")
 
 SET (CMAKE_CXX_COMPILER             "${DEVROOT}/usr/bin/clang++")
-SET (CMAKE_CXX_FLAGS_DEBUG          "-g")
-SET (CMAKE_CXX_FLAGS_MINSIZEREL     "-Os -DNDEBUG")
-SET (CMAKE_CXX_FLAGS_RELEASE        "-O4 -DNDEBUG")
-SET (CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g")
+SET (CMAKE_CXX_FLAGS_DEBUG          "-g -x objective-c++")
+SET (CMAKE_CXX_FLAGS_MINSIZEREL     "-Os -DNDEBUG -x objective-c++")
+SET (CMAKE_CXX_FLAGS_RELEASE        "-O4 -DNDEBUG -x objective-c++")
+SET (CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -x objective-c++")
 
-#SET (CMAKE_AR      "${DEVROOT}/usr/bin/ar")
 SET (CMAKE_AS      "${DEVROOT}/usr/bin/as")
 SET (CMAKE_LINKER  "${DEVROOT}/usr/bin/ld")
 SET (CMAKE_NM      "${DEVROOT}/usr/bin/nm")
 SET (CMAKE_RANLIB  "${DEVROOT}/usr/bin/ranlib")
 
 # Flags
-SET(OUR_FLAGS="-arch armv7 -mthumb -Wall -pipe -no-cpp-precomp -isysroot $SDKROOT -miphoneos-version-min=$IOS_DEPLOY_TGT -I$SDKROOT/usr/include/")
+SET(OUR_FLAGS="-arch armv7  -Wall -pipe -no-cpp-precomp -isysroot $SDKROOT -miphoneos-version-min=$IOS_DEPLOY_TGT -I${SDKROOT}/usr/include/")
 SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OUR_FLAGS}" CACHE STRING "c flags")
 SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OUR_FLAGS}" CACHE STRING "c++ flags")
 
+set(CMAKE_EXE_LINKER_FLAGS
+    "-framework OpenGLES -framework AudioToolbox -framework CoreGraphics -framework QuartzCore -framework UIKit"
+)
+
 # Header
 INCLUDE_DIRECTORIES(SYSTEM "${SDKROOT}/usr/include")
-INCLUDE_DIRECTORIES(SYSTEM "${SDKROOT}/usr/include/c++/4.2.1")
-INCLUDE_DIRECTORIES(SYSTEM "${SDKROOT}/usr/include/c++/4.2.1/armv7-apple-darwin10")
+#INCLUDE_DIRECTORIES(SYSTEM "${SDKROOT}/usr/include/c++/4.2.1")
+#INCLUDE_DIRECTORIES(SYSTEM "${SDKROOT}/usr/include/c++/4.2.1/armv7-apple-darwin10")
 INCLUDE_DIRECTORIES(SYSTEM "${SDKROOT}/System/Library/Frameworks")
 
 # System Libraries
@@ -77,9 +91,11 @@ LINK_DIRECTORIES("${SDKROOT}/usr/lib")
 LINK_DIRECTORIES("${SDKROOT}/System/Library/Frameworks")
 LINK_DIRECTORIES("${SDKROOT}/usr/lib/gcc/arm-apple-darwin10/4.2.1")
 
-SET (CMAKE_FIND_ROOT_PATH "${SDKROOT}")
-SET (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
-SET (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-SET (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH ${DEVROOT} ${SDKROOT})
 
-SET (CMAKE_CROSSCOMPILING 1)
+#SET (CMAKE_FIND_ROOT_PATH "${SDKROOT}")
+#SET (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
+#SET (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+#SET (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+#MESSAGE("CMAKE_OSX_SYSROOT: ${CMAKE_OSX_SYSROOT}")
